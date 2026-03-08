a73f09ed583d7676e5f725c0c4c22cff09098605
Miki Butler <dg1company@gmail.com>
2026-03-08 00:14:53 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/BlenderTutor/',  // 👈 replace with your exact repo name
+  base: '/BlenderTutor/',
+  build: {
+    outDir: 'docs',
+  },
 })
