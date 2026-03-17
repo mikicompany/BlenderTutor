de2dc06fcacc89aa5b60e0e84a38f79305da3ef0
Miki Butler <dg1company@gmail.com>
2026-03-17 16:10:50 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/BlenderTutor/',
-  build: {
-    outDir: 'docs',
-  },
 })
