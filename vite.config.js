4bb4e139197e7cb5df31f8884c600bddea868c09
Miki Butler <dg1company@gmail.com>
2026-04-11 23:06:14 -0700

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        tags: [
+          {
+            tag: 'script',
+            attrs: {},
+            children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MRSNFX33');`,
+            injectTo: 'head-prepend',
+          },
+        ],
+      },
+    }),
+  ],
+  base: '/',
 })
