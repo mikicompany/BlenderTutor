4cf72e56c4d3ddbe22072e7f22c98bfbca10dc8a
Miki Butler <dg1company@gmail.com>
2026-06-09 19:35:48 -0700

    Sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    sitemap({
+      hostname: 'https://www.blendertutoring.com',
+      exclude: ['/googlea46d0184849e086f']
+    })
+  ],
 })
